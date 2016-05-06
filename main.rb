a6717874c8ba35d657de7955d6e797a346a3c05c
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2016-05-06 12:05:58 +0100

    pass a variable to templates

--- main.rb
+++ main.rb
@@ -13,3 +13,7 @@ get '/use/variables/from/templates' do
 	@variable = 'read as instance variable'
 	erb :use_variables
 end
+
+get '/pass/variable/to/templates' do
+	erb :use_variables, locals: {variable: 'variable pass when render'}
+end
